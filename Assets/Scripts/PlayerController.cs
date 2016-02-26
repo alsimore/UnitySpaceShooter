131c4a25955464160de43b4689d7bd27e9e7f21b
alsimore <alsimore@gmail.com>
2016-02-26 13:21:15 +1000

    Completed tutorial 5, moving the player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
 public class PlayerController : MonoBehaviour 
 {
 	private Rigidbody rb;
 
 	public float speed;
+	public float tilt;
+	public Boundary boundary;
 
 	void Start ()
 	{
@@ -20,9 +28,13 @@ public class PlayerController : MonoBehaviour
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.velocity = movement * speed;
 
-		rb.position = new Vector3 (
-			Mathf.Clamp (rb.position.x, xMin, xMax), 
+		rb.position = new Vector3 
+		(
+			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax), 
 			0.0f, 
-			z);
+			Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
 	}
 }
