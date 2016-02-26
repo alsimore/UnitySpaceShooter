405f01c279e4c1950ca8a6332fc92cccf181d4d2
alsimore <alsimore@gmail.com>
2016-02-26 12:20:57 +1000

    Initial commit for Unity space shooter tutorial.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	private Rigidbody rb;
+
+	public float speed;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3 (
+			Mathf.Clamp (rb.position.x, xMin, xMax), 
+			0.0f, 
+			z);
+	}
+}
